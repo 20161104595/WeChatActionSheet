e0a281ff4a0ec5c6eb42faf934461138bfeb9dba
20161104595 <a20161104595@mac25.local>
2019-01-04 17:04:26 +0800

    Initial commit

---
+++ WeChatActionSheet/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  WeChatActionSheet
+//
+//  Created by soliloquy on 2017/10/12.
+//  Copyright © 2017年 soliloquy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func didClick(_ sender: UIButton) {
+        
+        let acVC = ActionSheetViewController(cellTitleList: ["保存", "收藏", "分享", "点赞"])!
+        acVC.valueBlock = { index in
+            print(index)
+        }
+        acVC.cellTitleColor = UIColor.orange
+        acVC.cellTitleFont = 17
+        acVC.titleString = "修改"
+        
+        present(acVC, animated: false, completion:  nil)
+
+    }
+    
+}
+
